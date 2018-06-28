f853b81141dd919b842cfbcad82e415e4aa819d0
qiuguanlin <578482558@qq.com>
2018-06-28 11:48:02 +0800

    0628

--- src/TeacherAndStudentmanagement/StudentInterface.java
+++ src/TeacherAndStudentmanagement/StudentInterface.java
@@ -1,7 +1,12 @@
 package teacherAndStudentManagement;
 
+/**
+ * @Description:
+ * @Author: QiuGuanLin
+ * @Date: 2018/6/27 10:20
+ */
 public interface StudentInterface {
-	public void setFee(double fee);
+    public double getFee();
 
-	public double getFee();
+    public void setFee(double fee);
 }
--- src/TeacherAndStudentmanagement/TeacherInterface.java
+++ src/TeacherAndStudentmanagement/TeacherInterface.java
@@ -1,7 +1,12 @@
 package teacherAndStudentManagement;
 
+/**
+ * @Description:
+ * @Author: QiuGuanLin
+ * @Date: 2018/6/27 10:22
+ */
 public interface TeacherInterface {
-	public void setPay(double pay);
+    public double getPay();
 
-	public double getPay();
+    public void setPay(double pay);
 }
